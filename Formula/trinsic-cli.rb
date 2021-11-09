427a974ddd25e780b182b9437ecf346e5c19217f
Tomislav Markovski <tmarkovski@gmail.com>
2021-11-09 16:50:11 +0000

    trinsic-cli: add 1.0.0 bottle.

--- Formula/trinsic-cli.rb
+++ Formula/trinsic-cli.rb
@@ -5,6 +5,12 @@ class TrinsicCli < Formula
   sha256 "35f1b17cd80ef52dd8e628380c7401bc1b8e818b63033cce0d5ad328558a71a6"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/trinsic-id/homebrew-tap/releases/download/trinsic-cli-1.0.0"
+    sha256 cellar: :any_skip_relocation, catalina:     "acc2222ce83e8684a0ae78a7187dc25016acba2a076c16b29679c4bc6f01b99c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d223086263bf62cfb1a1511fcb9a3124dd3ecbb8135c39336b95b4ae8e4dab52"
+  end
+
   depends_on "rust" => :build
 
   def install
