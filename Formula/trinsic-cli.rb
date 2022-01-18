e8cc926f9addf0e49061b85c5074c55697e0e36a
Scott Phillips <polygonguru@gmail.com>
2022-01-17 21:50:20 -0500

    trinsic-cli 1.3.0
    Closes #10.
    
    Signed-off-by: Tomislav Markovski <tmarkovski@gmail.com>

--- Formula/trinsic-cli.rb
+++ Formula/trinsic-cli.rb
@@ -1,12 +1,12 @@
 class TrinsicCli < Formula
   desc "Command-line interface for Trinsic Ecosystems APIs"
   homepage "https://docs-v2.trinsic.id/"
-  url "https://github.com/trinsic-id/sdk/releases/download/v1.2.0/trinsic-cli-1.2.2.tar.gz"
-  sha256 "677240331aa985a11364af2aea7881bc3cb1d7a3b2f7290d12dd4b56884a7cfd"
+  url "https://github.com/trinsic-id/sdk/releases/download/v1.3.0/trinsic-cli-1.3.0.tar.gz"
+  sha256 "e8cd1f0620fe52d4b8f544bec71d1612f752f669408bc2961049636b56c5f1a4"
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/trinsic-id/homebrew-tap/releases/download/trinsic-cli-1.2.2"
+    root_url "https://github.com/trinsic-id/homebrew-tap/releases/download/trinsic-cli-1.3.0"
     sha256 cellar: :any_skip_relocation, big_sur:      "96cc535856cee4d0d2e4bfeb5cb489baa53e89b8290adf6a4145e883f8e22e78"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "cfdba5acfd09a6deaf7579b13b97cdcb18c3b88895786cfa8b72c108d57032a9"
   end
