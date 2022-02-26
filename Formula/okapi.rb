43aa29bc9c0fff01aaa7f9e46ce101ff2e39c2b3
Tomislav Markovski <tmarkovski@gmail.com>
2022-02-26 10:01:11 -0500

    okapi 1.4.0
    Closes #14.
    
    Signed-off-by: Tomislav Markovski <tmarkovski@gmail.com>

--- Formula/okapi.rb
+++ Formula/okapi.rb
@@ -1,12 +1,12 @@
 class Okapi < Formula
   desc "Set of tools that support workflows for authentic data and identity management"
   homepage "https://github.com/trinsic-id/okapi/"
-  url "https://github.com/trinsic-id/okapi/releases/download/v1.2.0/okapi-1.2.0.tar.gz"
-  sha256 "54494573cdbba598d046062ab38e05afb8169b66a0ea59f63e67507920e08191"
+  url "https://github.com/trinsic-id/okapi/releases/download/v1.4.0/okapi-1.4.0.tar.gz"
+  sha256 "2a7578eaf3c12c41f2a7600369ebd3aca91f4992a785d66d4a0486c6fa75cf8d"
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/trinsic-id/homebrew-tap/releases/download/okapi-1.2.0"
+    root_url "https://github.com/trinsic-id/homebrew-tap/releases/download/okapi-1.4.0"
     sha256 cellar: :any,                 catalina:     "e56756e31e07ce3556a1f969c14ad7857882053d886d35833b573cfcea34350e"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "d010b687c63ae5e5fe20a41bf8aa0b60beb8a61054763b2f644c8fdda5a1f419"
   end
