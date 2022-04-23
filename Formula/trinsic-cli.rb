8b27098760a6535ab196280bbdbb84bf7340b3e5
Tomislav Markovski <tmarkovski@gmail.com>
2022-04-23 08:25:28 -0400

    trinsic-cli 1.4.2
    Update trinsic-cli.rb
    
    Closes #16.
    
    Signed-off-by: Tomislav Markovski <tmarkovski@gmail.com>

--- Formula/trinsic-cli.rb
+++ Formula/trinsic-cli.rb
@@ -1,8 +1,8 @@
 class TrinsicCli < Formula
   desc "Command-line interface for Trinsic Ecosystems APIs"
   homepage "https://docs-v2.trinsic.id/"
-  url "https://github.com/trinsic-id/sdk/releases/download/v1.4.0/trinsic-cli-1.4.0.tar.gz"
-  sha256 "de4664e749e0479413b859e2494569e2d9bb93205749cef1647e41cbe9bf96d6"
+  url "https://github.com/trinsic-id/sdk/releases/download/v1.4.0/trinsic-cli-1.4.2.tar.gz"
+  sha256 "a314139f7872b86d6cd93bc91e15bb9bbf0d521d41df3ac79cf0f79ad1dcf4ac"
   license "Apache-2.0"
 
   bottle do
