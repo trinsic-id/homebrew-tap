1a71f2e243f3872d14ccd90855b143f37b2dba65
Scott Phillips <polygonguru@gmail.com>
2022-06-29 18:07:34 +0000

    trinsic-cli: update 1.6.0 bottle.

--- Formula/trinsic-cli.rb
+++ Formula/trinsic-cli.rb
@@ -6,9 +6,9 @@ class TrinsicCli < Formula
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/trinsic-id/homebrew-tap/releases/download/trinsic-cli-1.5.0"
-    sha256 cellar: :any_skip_relocation, big_sur:      "628fa4362228558a543c0297b4630c334c0ddf33ed06525c4f4efc17f7980332"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "b0bea4826435f683e355b831a756e8cb656fdcce68c67acfd3c2e713f6498267"
+    root_url "https://github.com/trinsic-id/homebrew-tap/releases/download/trinsic-cli-1.6.0"
+    sha256 cellar: :any_skip_relocation, big_sur:      "de2112eb5104e002f27c0a6a4b0b8a3d691c48d32ca35985cb76d0892387b484"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "667cd9416138ff4e8e8c7112fa009164e7b3892ff55269c27a549d7c688bb0b1"
   end
 
   depends_on "rust" => :build
